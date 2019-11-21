a8daf77f95a7dd68fea3b47a2e10b9500800d756
John <spandexerjd@gmail.com>
2019-11-20 19:12:02 -0800

    added req.body to post and patch routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,19 +15,25 @@ router.get("/", (req, res) => {
 
 // re-test this
 router.post("/api/burgers", (req, res) => {
-  burgers.insertOne(["burger_name"], ["Big Mac"], data => {
+  // need check for body null on front end (maybe backend?)
+  console.log("burger name: ", req.body);
+  burgers.insertOne(["burger_name"], [req.body.burger_name], data => {
     console.log(data);
-    res.json({ id: data });
+    res.json({ id: data.insertId });
   });
 });
 
 // change this to post
 router.patch("/api/burgers/:id", (req, res) => {
   burgers.updateOne(
-    { burger_name: "Veggie Burger" },
+    { burger_name: req.body.burger_name },
     "id = " + req.params.id,
     data => {
-      res.json({ id: data });
+      if (data.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
     }
   );
 });
