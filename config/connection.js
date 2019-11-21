9214e346d0f45949dd83ef00bc764ef31431109e
John Doan <doanja@plu.edu>
2019-11-21 08:48:14 -0800

    added JAWSDB_URL env variable for heroku

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,24 @@ const mysql = require("mysql");
  * function to establish connection with the SQL database
  */
 const initDBConnection = () => {
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "test",
-    database: "burgers_db"
-  });
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    return connection;
+  } else {
+    const connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "test",
+      database: "burgers_db"
+    });
 
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("SQL DB Connected!");
-  });
+    connection.connect(function(err) {
+      if (err) throw err;
+      console.log("SQL DB Connected!");
+    });
 
-  return connection;
+    return connection;
+  }
 };
 
 module.exports = initDBConnection;
