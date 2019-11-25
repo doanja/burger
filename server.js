d769bf876b6b0aa5f1eaead85bed356d17fb5be8
John <doanja@plu.edu>
2019-11-24 16:14:22 -0800

    updated project and html files to link to public folder

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 // Sets up the Express app to handle data parsing (needed to process POST request)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
